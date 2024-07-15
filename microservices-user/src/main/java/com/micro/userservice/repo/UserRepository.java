16db51dea41a397d3b141ea0b1385b880137c14f
Akhtarvaseem <99640371+Akhtarvaseem@users.noreply.github.com>
2024-07-15 18:30:46 +0530

    Delete microservices-user directory

--- microservices-user/src/main/java/com/micro/userservice/repo/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.micro.userservice.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.micro.userservice.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Integer>{
-
-}
